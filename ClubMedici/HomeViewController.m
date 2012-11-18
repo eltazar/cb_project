d6ad35150d67a1efe510cfdcafb5eacd9cc90b38
Mario Greco <mrgreco3@gmail.com>
2012-11-18 17:04:58 +0100

    [HomeViewController +  xibs]: prime prove con interfaccia home

--- ClubMedici/HomeViewController.m
+++ ClubMedici/HomeViewController.m
@@ -19,23 +19,6 @@
 {
     [super viewDidLoad];
     
-    //se commento da qui in poi il menu laterale salta fuori solo premendo il bottone e non più con la gesture
-    
-    CGFloat red = (CGFloat)arc4random() / 0x100000000;
-    CGFloat green = (CGFloat)arc4random() / 0x100000000;
-    CGFloat blue = (CGFloat)arc4random() / 0x100000000;
-    self.view.backgroundColor = [UIColor whiteColor];
-    
-//    UILabel *label  = [[UILabel alloc] init];
-//    label.font = [UIFont boldSystemFontOfSize:20.0f];
-//    label.text = @"Center Panel";
-//    [label sizeToFit];
-//    label.center = CGPointMake(floorf(self.view.bounds.size.width/2.0f), floorf((self.view.bounds.size.height - self.navigationController.navigationBar.frame.size.height)/2.0f));
-//    label.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
-//    [self.view addSubview:label];
-
-    //[self.view addSubview:self.leftPanel.view];
-    
 }
 
 - (void)didReceiveMemoryWarning
