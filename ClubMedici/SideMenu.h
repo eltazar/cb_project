b625dd5b8d9e74259f92958a495ef41f167a9bea
Mario Greco <mrgreco3@gmail.com>
2012-11-18 18:17:45 +0100

    [rinominazione classi]

---
+++ ClubMedici/SideMenu.h
@@ -0,0 +1,13 @@
+//
+//  SideMenu.h
+//  ClubMedici
+//
+//  Created by mario greco on 18/11/12.
+//  Copyright (c) 2012 mario greco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideMenu : UITableViewController
+
+@end
