1f89985e76e466e694daa9680a3ff54673e03d9f
Mario Greco <mrgreco3@gmail.com>
2012-11-19 12:53:48 +0100

    [SideMenuController_iPhone]: classe specializzata per le funzioni iphone

---
+++ ClubMedici/SideMenuController_iPhone.h
@@ -0,0 +1,13 @@
+//
+//  SideMenuController_iPhone.h
+//  ClubMedici
+//
+//  Created by mario greco on 19/11/12.
+//  Copyright (c) 2012 mario greco. All rights reserved.
+//
+
+#import "SideMenuController.h"
+
+@interface SideMenuController_iPhone : SideMenuController
+
+@end
