40524d49d5f67d8516c3edabb1e9ac1a9c84b61a
Mario Greco <mrgreco3@gmail.com>
2012-11-20 22:55:03 +0100

    [AreaBaseController]: prova di interfaccia per l'area di base

---
+++ AreaBaseController.h
@@ -0,0 +1,18 @@
+//
+//  AreaBaseControllerViewController.h
+//  ClubMedici
+//
+//  Created by mario greco on 20/11/12.
+//  Copyright (c) 2012 mario greco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AreaBase;
+@interface AreaBaseController : UITableViewController
+
+@property(nonatomic, strong) AreaBase *area;
+@property (nonatomic, assign) IBOutlet UITableViewCell *areaDescriptionCell;
+
+- (id) initWithArea:(AreaBase*)area;
+@end
---
+++ AreaBaseController.m
@@ -0,0 +1,179 @@
+//
+//  AreaBaseControllerViewController.m
+//  ClubMedici
+//
+//  Created by mario greco on 20/11/12.
+//  Copyright (c) 2012 mario greco. All rights reserved.
+//
+
+#import "AreaBaseController.h"
+
+@interface AreaBaseController ()
+
+@end
+
+@implementation AreaBaseController
+@synthesize areaDescriptionCell;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (id) initWithArea:(AreaBase*)area{
+   
+    self = [super init];
+  
+    if(self){
+        self.area = area;
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    switch (section) {
+        case 0:
+            return 2;
+            break;
+        case 1:
+            return 1;
+            break;
+        case 2:
+            return 1;
+            break;
+        default:
+            return 0;
+            break;
+    }
+
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{    
+    UITableViewCell *cell;
+    
+    if(indexPath.section == 0 && indexPath.row == 0){
+        static NSString * cellIdentifier = @"DescriptionCell";
+        cell = [tableView dequeueReusableCellWithIdentifier: cellIdentifier];
+        if (cell == nil) {
+            [[NSBundle mainBundle] loadNibNamed:@"AreaDescriptionCell" owner:self options:nil];
+            cell = areaDescriptionCell;
+            // Configure the cell
+            
+            self.areaDescriptionCell = nil;
+        }
+        // Configure the cell
+
+    }
+    else{
+    
+        static NSString *CellIdentifier = @"Cell";
+        cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        if (cell == nil) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+        }
+        cell.textLabel.text = @"ciao";
+    }
+
+    // Configure the cell...
+
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return @"";
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.section == 0 && indexPath.row == 0)
+        return 131;
+    return [super tableView:tableView heightForRowAtIndexPath:indexPath];
+}
+
+@end
