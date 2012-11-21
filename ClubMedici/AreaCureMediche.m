3b1665527fb869d3de132e7b24ccbbbf31b304e5
Mario Greco <mrgreco3@gmail.com>
2012-11-21 14:52:50 +0100

    [AreaCureMediche]: implementato getModel

--- ClubMedici/AreaCureMediche.m
+++ ClubMedici/AreaCureMediche.m
@@ -28,9 +28,65 @@
         [self.pdfList addObject:[[NSDictionary alloc] initWithObjectsAndKeys:@"link1",@"Titolo1", nil]];
         [self.pdfList addObject:[[NSDictionary alloc] initWithObjectsAndKeys:@"link2",@"Titolo2", nil]];
         [self.pdfList addObject:[[NSDictionary alloc] initWithObjectsAndKeys:@"link3",@"Titolo3", nil]];
-        
     }
     return self;
+}
+
+-(NSMutableDictionary *) getDataModel{
+    /*
+     STRUTTURA:
+     
+     DIZIONARIO DATA MODEL:
+     k -> sections
+     v -> titolo delle sezioni
+     k -> data
+     v -> array di dati per ogni sezione
+     
+     per la chiave DATA: ho array tale che:
+     indice 0 -> sezione 0:
+     elementi sono stringhe come: descrizione, telefono,  mail....
+     indice 1 -> sezione 1:
+     elementi sono dizionari: k -> titolo del link
+     v -> url
+     */
+    
+    
+    //data model totale
+    NSMutableDictionary *dataModel = [[NSMutableDictionary alloc] init];
+    
+    //array di dati  per le varie sezioni
+    NSMutableArray *data = [[NSMutableArray alloc] initWithCapacity:3];
+    
+    //array di titoli sezione
+    [dataModel setObject:[NSArray arrayWithObjects:@"Informazioni",@"Documenti",@"Simulatore rate", nil] forKey:@"sections"];
+    
+    //array di dati per la sezione 0
+    NSMutableArray *info = [[NSMutableArray alloc] init];
+    
+    if(self.descrizione != nil)
+        [info addObject:self.descrizione];
+    if(self.tel != nil)
+        [info addObject:self.tel];
+    
+    
+    [data insertObject:info atIndex:0];
+    
+    //array di dati per la sezione 1
+    [data insertObject:self.pdfList atIndex:1];
+    
+    
+    //array di dati per la sezione 2
+    NSMutableArray *preventivo = [[NSMutableArray alloc] initWithCapacity:3];
+    [preventivo addObject:@"Calcola rata"];
+    
+    [data insertObject:preventivo atIndex:2];
+    
+    [dataModel setObject:data forKey:@"data"];
+    
+    NSLog(@"DATA MODEL = %@",dataModel);
+    
+    return dataModel;
+
 }
 
 @end
