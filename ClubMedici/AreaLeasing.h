83a3330af90883dee5da2425c51c4e36ff06666d
Mario Greco <mrgreco3@gmail.com>
2012-11-21 14:36:39 +0100

    [AreaLeasing]: dimenticavo .h

---
+++ ClubMedici/AreaLeasing.h
@@ -0,0 +1,13 @@
+//
+//  AreaLeasing.h
+//  ClubMedici
+//
+//  Created by mario greco on 21/11/12.
+//  Copyright (c) 2012 mario greco. All rights reserved.
+//
+
+#import "AreaBase.h"
+
+@interface AreaLeasing : AreaBase
+
+@end
