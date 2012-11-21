6c492cf6ba978513c1ef7734bc1456a8c67c7717
Mario Greco <mrgreco3@gmail.com>
2012-11-21 16:25:50 +0100

    [AreaBaseController_iPad]: per ora inserito, forse serve in futuro per gestire la grafica dell'interfaccia ipad relativa alle aree

---
+++ ClubMedici/AreaBaseController_iPad.h
@@ -0,0 +1,13 @@
+//
+//  AreaBaseController_iPad.h
+//  ClubMedici
+//
+//  Created by mario greco on 21/11/12.
+//  Copyright (c) 2012 mario greco. All rights reserved.
+//
+
+#import "AreaBaseController.h"
+
+@interface AreaBaseController_iPad : AreaBaseController
+
+@end
---
+++ ClubMedici/AreaBaseController_iPad.m
@@ -0,0 +1,38 @@
+//
+//  AreaBaseController_iPad.m
+//  ClubMedici
+//
+//  Created by mario greco on 21/11/12.
+//  Copyright (c) 2012 mario greco. All rights reserved.
+//
+
+#import "AreaBaseController_iPad.h"
+
+@interface AreaBaseController_iPad ()
+
+@end
+
+@implementation AreaBaseController_iPad
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
