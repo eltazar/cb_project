937d39b947bb9e6374215dc31d9dee7a87e28804
Mario Greco <mrgreco3@gmail.com>
2012-11-23 12:03:31 +0100

    [Celle custom]: celle custom per il form di richiesta

---
+++ TextFieldCell.h
@@ -0,0 +1,13 @@
+//
+//  TextFieldCell.h
+//  ClubMedici
+//
+//  Created by mario greco on 22/11/12.
+//  Copyright (c) 2012 mario greco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextFieldCell : UITableViewCell
+
+@end
---
+++ TextFieldCell.m
@@ -0,0 +1,53 @@
+//
+//  TextFieldCell.m
+//  ClubMedici
+//
+//  Created by mario greco on 22/11/12.
+//  Copyright (c) 2012 mario greco. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+#import "TextFieldCell.h"
+
+@interface TextFieldCell(){
+    UITextField *textField;
+}
+
+@end
+
+@implementation TextFieldCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+-(void)layoutSubviews{
+    [super layoutSubviews];
+    
+    textField = (UITextField*)[self viewWithTag:1];
+    
+    //setto il bottom border per il textField
+    CALayer *bottomBorder = [CALayer layer];
+    
+    bottomBorder.frame = CGRectMake(0.0f, textField.frame.size.height-10, textField.frame.size.width, 1.5f);
+    
+    bottomBorder.backgroundColor = [UIColor grayColor].CGColor;
+    bottomBorder.cornerRadius = 2;
+    
+    [textField.layer addSublayer:bottomBorder];
+}
+
+@end
