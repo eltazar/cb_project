879ec824ff3e24650c3e803890891b0bb2580ad5
mario <mario@marios-Mac.local>
2013-04-23 01:17:26 +0200

    PDHttpAcces+WMHttpAccess]: inseriti

---
+++ ClubMedici/PDHTTPAccess.h
@@ -0,0 +1,32 @@
+//
+//  PDHTTPAccess.h
+//  PerDueCItyCard
+//
+//  Created by Gabriele "Whisky" Visconti on 11/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WMHTTPAccess.h"
+
+
+@interface PDHTTPAccess : NSObject
+
++ (void)getAreaContents:(int)areaId delegate:(id<WMHTTPAccessDelegate>)delegate;
+
+
++ (void)checkUserFields:(NSArray*)usr delegate:(id<WMHTTPAccessDelegate>)delegate;
+
++ (void)sendRetrievePswForUser:(NSString*)usr delegate:(id<WMHTTPAccessDelegate>)delegate;
+
++ (void)buyCouponRequest:(NSString*)string delegate:(id<WMHTTPAccessDelegate>)delegate;
+
++ (void)requestACard:(NSArray*)data delegate:(id<WMHTTPAccessDelegate>)delegate;
+
++ (void)getAltreOfferteFromServer:(NSString*)prov delegate:(id<WMHTTPAccessDelegate>)delegate;
+
++ (void)getCouponFromServerWithId:(NSInteger)idCoupon delegate:(id<WMHTTPAccessDelegate>) delegate ;
+
++ (void)getCouponFromServer:(NSString*)prov delegate:(id<WMHTTPAccessDelegate>)delegate;
+
+@end
---
+++ ClubMedici/PDHTTPAccess.m
@@ -0,0 +1,123 @@
+//
+//  PDHTTPAccess.m
+//  PerDueCItyCard
+//
+//  Created by Gabriele "Whisky" Visconti on 11/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PDHTTPAccess.h"
+
+@implementation PDHTTPAccess
+
+
++ (void)getAreaContents:(int)areaId delegate:(id<WMHTTPAccessDelegate>)delegate {
+    //NSLog(@"DBACCESS REGISTER  --> user = %@", userData);
+    
+    NSLog(@"AREA ID = %d",areaId);
+    NSString *urlString = @"http://www.clubmedici.it/app/iphone/ContenutiAree.php";
+    
+    NSDictionary *postDict = [NSDictionary dictionaryWithObjectsAndKeys:
+                             [NSNumber numberWithInt:areaId], @"areaId",
+                              nil];
+    [[WMHTTPAccess sharedInstance] startHTTPConnectionWithURLString:urlString method:WMHTTPAccessConnectionMethodPOST parameters:postDict delegate:delegate];
+}
+
+/*
++ (void)checkUserFields:(NSArray*)usr delegate:(id<WMHTTPAccessDelegate>)delegate {
+    //NSLog(@"DBACCESS CHECK  EMAIL --> user = %@",usr);
+    
+    NSString *urlString = @"";
+    NSDictionary *postDict = nil;
+    
+    if (usr.count == 1) {
+        urlString = @"https://cartaperdue.it/partner/checkEmail.php";
+        postDict = [NSDictionary dictionaryWithObjectsAndKeys:
+                    [usr objectAtIndex:0], @"usr", nil];
+    }
+    else if (usr.count == 2) {
+        urlString = @"https://cartaperdue.it/partner/login.php";
+        postDict = [NSDictionary dictionaryWithObjectsAndKeys:
+                    [usr objectAtIndex:0], @"usr",
+                    [usr objectAtIndex:1], @"psw",
+                    nil];
+    }
+    [[WMHTTPAccess sharedInstance] startHTTPConnectionWithURLString:urlString method:WMHTTPAccessConnectionMethodPOST parameters:postDict delegate:delegate];
+}
+
+
++ (void)sendRetrievePswForUser:(NSString*)usr delegate:(id<WMHTTPAccessDelegate>)delegate {
+    //NSLog(@"DBACCESS RECUPERA PASSWORD");
+    
+    NSString *urlString = @"http://www.cartaperdue.it/partner/recuperaPsw.php";
+    
+    NSDictionary *postDict =[NSDictionary dictionaryWithObjectsAndKeys:
+                             usr, @"usr",
+                             nil];
+    [[WMHTTPAccess sharedInstance] startHTTPConnectionWithURLString:urlString method:WMHTTPAccessConnectionMethodPOST parameters:postDict delegate:delegate];
+}
+
+
++ (void)buyCouponRequest:(NSString*)string delegate:(id<WMHTTPAccessDelegate>)delegate {
+    NSString *urlString = @"https://cartaperdue.it/partner/acquistoCoupon.php";
+    
+    NSMutableDictionary *postDict = [NSMutableDictionary dictionaryWithCapacity:10];
+    NSArray *pairs = [string componentsSeparatedByString:@"&"];
+    for (NSString *pair in pairs) {
+        NSArray *keyValue = [pair componentsSeparatedByString:@"="];
+        [postDict setObject:[keyValue objectAtIndex:1] forKey:[keyValue objectAtIndex:0]];
+    }
+    NSLog(@"[%@ buyCouponRequest] string = [%@], \n\tpostDict = %@", [self class], string, postDict);
+    
+    [[WMHTTPAccess sharedInstance] startHTTPConnectionWithURLString:urlString method:WMHTTPAccessConnectionMethodPOST parameters:postDict delegate:delegate];
+}
+
++ (void)requestACard:(NSArray*)data delegate:(id<WMHTTPAccessDelegate>)delegate {
+    
+    NSString *urlString = @"https://cartaperdue.it/partner/v2.0/RichiediCarta.php";
+    
+    
+    NSDictionary *postDict =[NSDictionary dictionaryWithObjectsAndKeys:
+                             [data objectAtIndex:0], @"cardType",
+                             [data objectAtIndex:1], @"name",
+                             [data objectAtIndex:2], @"surname",
+                             [data objectAtIndex:3], @"phone",
+                             [data objectAtIndex:4], @"email",
+                             nil];
+    
+    [[WMHTTPAccess sharedInstance] startHTTPConnectionWithURLString:urlString method:WMHTTPAccessConnectionMethodPOST parameters:postDict delegate:delegate];
+}
+
+
++ (void)getAltreOfferteFromServer:(NSString*)prov delegate:(id<WMHTTPAccessDelegate>)delegate {
+    //NSLog(@"QUERY: altre offerte");
+    
+    NSString *urlString = [NSString stringWithFormat: @"http://www.cartaperdue.it/partner/altreofferte.php?prov=%@", prov];
+    [[WMHTTPAccess sharedInstance] startHTTPConnectionWithURLString:urlString method:WMHTTPAccessConnectionMethodGET parameters:nil delegate:delegate];
+}
+
++ (void)getCouponFromServerWithId:(NSInteger)idCoupon delegate:(id<WMHTTPAccessDelegate>) delegate {
+   // NSLog(@"QUERY PER COUPON CON ID");
+    
+	NSString *urlString = [NSString stringWithFormat: @"http://www.cartaperdue.it/partner/offerta2.php?id=%d", idCoupon];
+    [[WMHTTPAccess sharedInstance] startHTTPConnectionWithURLString:urlString method:WMHTTPAccessConnectionMethodGET parameters:nil delegate:delegate];
+}
+
++ (void)getCouponFromServer:(NSString*)prov delegate:(id<WMHTTPAccessDelegate>)delegate {
+    //NSLog(@"QUERY PER COUPON");
+    
+    NSString *urlString = [NSString stringWithFormat:@"http://www.cartaperdue.it/partner/coupon2.php?prov=%@",prov];
+    [[WMHTTPAccess sharedInstance] startHTTPConnectionWithURLString:urlString method:WMHTTPAccessConnectionMethodGET parameters:nil delegate:delegate];
+}
+
+
+
+*/
+@end
+
+
+
+
+
+
+
