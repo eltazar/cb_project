78d040a4a05d210e3a3fd54bd77494a5d0b3af75
mario <mario@marios-Mac.local>
2013-04-23 01:19:44 +0200

    [AreaEnum]:enum delle aree

---
+++ ClubMedici/AreeEnum.h
@@ -0,0 +1,21 @@
+//
+//  ConnectionTagsEnum.h
+//  ClubMedici
+//
+//  Created by mario on 23/04/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#ifndef ClubMedici_AreeEnum_h
+#define ClubMedici_AreeEnum_h
+
+enum areeEnum
+{
+    AreaFinanziaria = 11,
+    AreaAssicurativa = 12,
+    AreaCureMediche = 13,
+    AreaLeasing = 14,
+    AreaTurismo = 15,
+};
+typedef enum areeEnum AreeEnum;
+#endif
