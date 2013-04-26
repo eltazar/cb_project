9b5cf0fe1453dcd977c91b285bbc2d8ddef50e7f
mario <mario@marios-Mac.local>
2013-04-26 13:54:52 +0200

    [PullableView]: libreria per le view trascinabili o toggle-abili

---
+++ ClubMedici/NewsPullableView.h
@@ -0,0 +1,13 @@
+//
+//  NewsPullableView.h
+//  ClubMedici
+//
+//  Created by mario on 26/04/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import "PullableView.h"
+
+@interface NewsPullableView : PullableView
+
+@end
---
+++ ClubMedici/NewsPullableView.m
@@ -0,0 +1,31 @@
+//
+//  NewsPullableView.m
+//  ClubMedici
+//
+//  Created by mario on 26/04/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import "NewsPullableView.h"
+
+@implementation NewsPullableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
