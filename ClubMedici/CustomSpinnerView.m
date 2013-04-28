d5d35a88611346436e5da6622dc6d64ef5e17390
mario <mario@marios-Mac.local>
2013-04-28 17:02:57 +0200

    [CustomSpinnerView]: cambiate immagini

--- ClubMedici/CustomSpinnerView.m
+++ ClubMedici/CustomSpinnerView.m
@@ -23,30 +23,23 @@
         
         //Add more images which will be used for the animation
         self.animationImages = [NSArray arrayWithObjects:
-                                             [UIImage imageNamed:@"spinner30.png"],
-                                             [UIImage imageNamed:@"spinner60.png"],
-                                             [UIImage imageNamed:@"spinner90.png"],
-                                             [UIImage imageNamed:@"spinner120.png"],
-                               [UIImage imageNamed:@"spinner150.png"],
-        [UIImage imageNamed:@"spinner180.png"],
-        [UIImage imageNamed:@"spinner210.png"],
-        [UIImage imageNamed:@"spinner240.png"],
-                                [UIImage imageNamed:@"spinner270.png"],
-        [UIImage imageNamed:@"spinner300.png"],
-        [UIImage imageNamed:@"spinner330.png"],
-                                 [UIImage imageNamed:@"spinner360.png"],
+                                             [UIImage imageNamed:@"spinner1.png"],
+                                             [UIImage imageNamed:@"spinner2.png"],
+                                             [UIImage imageNamed:@"spinner3.png"],
+                                             [UIImage imageNamed:@"spinner4.png"],
+                                             [UIImage imageNamed:@"spinner5.png"],
                                              nil];
         
         self.frame = CGRectMake( frame.size.width/2
                                              -statusImage.size.width/2,
-                                             frame.size.height/2
-                                             -statusImage.size.height/2,
+                                             (frame.size.height/2
+                                             -statusImage.size.height/2),
                                              statusImage.size.width,
                                              statusImage.size.height);
         
         //Set the duration of the animation (play with it
         //until it looks nice for you)
-        self.animationDuration = 1.1;
+        self.animationDuration = 2.5;
     }
     return self;
 }
