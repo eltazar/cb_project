a822ec171d9f6d9feb34e6841cbc54aa6971a161
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2013-05-04 03:09:19 +0200

    [AreaTurismo] Controller

---
+++ ClubMedici/AreaTurismoController.h
@@ -0,0 +1,13 @@
+//
+//  AreaTurismoController.h
+//  ClubMedici
+//
+//  Created by Gabriele "Whisky" Visconti on 03/05/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import "AreaBaseController.h"
+
+@interface AreaTurismoController : AreaBaseController
+
+@end
---
+++ ClubMedici/AreaTurismoController.m
@@ -0,0 +1,74 @@
+//
+//  AreaTurismoController.m
+//  ClubMedici
+//
+//  Created by Gabriele "Whisky" Visconti on 03/05/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import "AreaTurismoController.h"
+#import "WMTableViewDataSource.h"
+
+@interface AreaTurismoController ()
+
+@end
+
+@implementation AreaTurismoController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	_dataModel = [[WMTableViewDataSource alloc] initWithPList:@"AreaTurismo"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - Table view data source
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = nil;
+    //NSString *cellIdentifier;
+    NSString *dataKey = [_dataModel valueForKey:@"DATA_KEY" atIndexPath:indexPath];
+    
+    if(indexPath.section == 0){
+        
+        cell = [tableView dequeueReusableCellWithIdentifier:@"ContactCell"];
+        if (!cell) {
+            cell = [[[NSBundle mainBundle] loadNibNamed:@"ContactCell" owner:self options:NULL] objectAtIndex:0];
+            UIView *v = [[UIView alloc] init];
+            v.backgroundColor = [UIColor colorWithRed:210/255.0f green:230/255.0f blue:236/255.0f alpha:1];
+            cell.selectedBackgroundView = v;
+        }
+        
+        UILabel *contactLabel = (UILabel *) [cell viewWithTag:1];
+        //        contactLabel.shadowColor = [UIColor colorWithWhite:1 alpha:1];
+        //        contactLabel.shadowOffset = CGSizeMake(1.0f, 1.0f);
+        //        contactLabel.shadowBlur = 1.0f;
+        //        contactLabel.innerShadowBlur = 3.0f;
+        //        contactLabel.innerShadowColor = [UIColor colorWithRed:22/255.0f green:47/255.0f blue:156/255.0f alpha:1];
+        //        contactLabel.innerShadowOffset = CGSizeMake(1.0f, 1.0f);
+        contactLabel.textColor = [UIColor colorWithRed:28/255.0f green:60/255.0f blue:119/255.0f alpha:1];
+        
+        contactLabel.highlightedTextColor =[UIColor blackColor];
+        
+        UIImageView *img = (UIImageView *)[cell viewWithTag:2];
+    }
+}
+
+
+@end
