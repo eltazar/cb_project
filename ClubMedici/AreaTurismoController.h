688feb664ef49e0dc0faf515f865c570ae5c071f
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2013-05-06 02:56:22 +0200

    [AreaTurismoController] classe aggiunta per compatibilità con UIViewController+InterfaceIdiom

---
+++ ClubMedici/AreaTurismoController.h
@@ -0,0 +1,19 @@
+//
+//  AreaTurismoController.h
+//  ClubMedici
+//
+//  Created by Gabriele "Whisky" Visconti on 06/05/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+/*
+ Questa classe, seppur di fatto vuota, occorre per il corretto funzionamento del
+ meccanismo di idiomAllocInit
+ */
+
+
+#import "AreaBaseController.h"
+
+@interface AreaTurismoController : AreaBaseController
+
+@end
---
+++ ClubMedici/AreaTurismoController.m
@@ -0,0 +1,38 @@
+//
+//  AreaTurismoController.m
+//  ClubMedici
+//
+//  Created by Gabriele "Whisky" Visconti on 06/05/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import "AreaTurismoController.h"
+
+@interface AreaTurismoController ()
+
+@end
+
+@implementation AreaTurismoController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
