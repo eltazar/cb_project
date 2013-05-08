cf7fbfb1fa66e5a280a0e6317f51366a23b9dfc3
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2013-05-08 03:48:07 +0200

    [AreaTurismoSection] initialization methods

--- ClubMedici/AreaTurismoSection.h
+++ ClubMedici/AreaTurismoSection.h
@@ -7,7 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BusinessLogicBase.h"
 
-@interface AreaTurismoSection : NSObject
+@interface AreaTurismoSection : BusinessLogicBase
+
+@property (nonatomic, assign) NSInteger sectionId;
 
 @end
--- ClubMedici/AreaTurismoSection.m
+++ ClubMedici/AreaTurismoSection.m
@@ -7,7 +7,44 @@
 //
 
 #import "AreaTurismoSection.h"
+#import "AreaTurismoItem.h"
+#import "PDHTTPAccess.h"
+
+@interface AreaTurismoSection () {
+    NSMutableArray *_items;
+}
+@end
+
+
 
 @implementation AreaTurismoSection
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        _items = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+
+- (void)fetchData {
+    [PDHTTPAccess getAreaTurismoDataForSectionId:self.sectionId inItaly:YES delegate:self];
+}
+
+
+
+#pragma mark - WMHTTPAccessDelegate
+
+
+
+- (void)didReceiveJSON:(NSArray *)jsonArray {
+    NSArray *tempArray;
+    for (NSDictionary *item in jsonArray) {
+        tempArray = [NSArray arrayWithObject:item];
+        [_items addObject:[[AreaTurismoItem alloc] initWithJson:tempArray]];
+    }
+    [self.delegate didReceiveBusinessLogicData];
+}
 
 @end
