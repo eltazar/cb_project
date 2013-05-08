fe5e18a3f6bba13e03e7894ca0f0067b7d225fda
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2013-05-08 02:22:02 +0200

    [AreaTurismoSection] Classe Aggiunta

---
+++ ClubMedici/AreaTurismoSection.h
@@ -0,0 +1,13 @@
+//
+//  AreaTurismoSection.h
+//  ClubMedici
+//
+//  Created by Gabriele "Whisky" Visconti on 08/05/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AreaTurismoSection : NSObject
+
+@end
---
+++ ClubMedici/AreaTurismoSection.m
@@ -0,0 +1,13 @@
+//
+//  AreaTurismoSection.m
+//  ClubMedici
+//
+//  Created by Gabriele "Whisky" Visconti on 08/05/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import "AreaTurismoSection.h"
+
+@implementation AreaTurismoSection
+
+@end
