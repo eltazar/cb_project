4e77707313220c013228cdf1a6be31cea08b1e47
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2013-05-08 03:48:35 +0200

    [AreaTurismoItem] classe aggiunta

---
+++ ClubMedici/AreaTurismoItem.h
@@ -0,0 +1,13 @@
+//
+//  AreaTurismoItem.h
+//  ClubMedici
+//
+//  Created by Gabriele "Whisky" Visconti on 08/05/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import "BusinessLogicBase.h"
+
+@interface AreaTurismoItem : BusinessLogicBase
+
+@end
---
+++ ClubMedici/AreaTurismoItem.m
@@ -0,0 +1,13 @@
+//
+//  AreaTurismoItem.m
+//  ClubMedici
+//
+//  Created by Gabriele "Whisky" Visconti on 08/05/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import "AreaTurismoItem.h"
+
+@implementation AreaTurismoItem
+
+@end
