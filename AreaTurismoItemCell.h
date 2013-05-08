bb8fdb921547f5c4c5c360324c7c32e59fb43c53
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2013-05-08 14:02:55 +0200

    [AreaTurismoItemCell] classe aggiunta

---
+++ AreaTurismoItemCell.h
@@ -0,0 +1,13 @@
+//
+//  AreaTurismoItemCell.h
+//  ClubMedici
+//
+//  Created by Gabriele "Whisky" Visconti on 08/05/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AreaTurismoItemCell : UITableViewCell
+
+@end
---
+++ AreaTurismoItemCell.m
@@ -0,0 +1,29 @@
+//
+//  AreaTurismoItemCell.m
+//  ClubMedici
+//
+//  Created by Gabriele "Whisky" Visconti on 08/05/13.
+//  Copyright (c) 2013 mario greco. All rights reserved.
+//
+
+#import "AreaTurismoItemCell.h"
+
+@implementation AreaTurismoItemCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
