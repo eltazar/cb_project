9fff4da12904a45eee3fea75ef2210ba549b8ec9
Gabriele "Whisky" Visconti <visconti.gabriele@gmail.com>
2013-05-09 02:36:09 +0200

    [TurismoTableViewController] fix varii

--- ClubMedici/TurismoTableViewController.m
+++ ClubMedici/TurismoTableViewController.m
@@ -37,6 +37,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    /*self.areaTurismoSection = [[AreaTurismoSection alloc] init];
+    self.areaTurismoSection.sectionId = self*/
     self.areaTurismoSection.delegate = self;
     [self.areaTurismoSection fetchData];
     
@@ -79,125 +81,11 @@
         UIView *v = [[UIView alloc] init];
         v.opaque = YES;
         v.backgroundColor = [UIColor colorWithRed:194/255.0f green:203/255.0f blue:219/255.0f alpha:1];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
         cell.selectedBackgroundView = v;
     }
     ((AreaTurismoItemCell *)cell).areaTurismoItem = [_dataModel valueForKey:@"ITEM" atIndexPath:indexPath];
     return cell;
-    
-    
-    /*UITableViewCell *cell = nil;
-    //NSString *cellIdentifier;
-    NSString *dataKey = [_dataModel valueForKey:@"DATA_KEY" atIndexPath:indexPath];
-    
-    if (indexPath.section == 0) {
-        cell = [tableView dequeueReusableCellWithIdentifier:@"ContactCell"];
-        if (!cell) {
-            cell = [[[NSBundle mainBundle] loadNibNamed:@"ContactCell" owner:self options:NULL] objectAtIndex:0];
-            UIView *v = [[UIView alloc] init];
-            v.opaque = YES;
-            v.backgroundColor = [UIColor colorWithRed:194/255.0f green:203/255.0f blue:219/255.0f alpha:1];
-            cell.selectedBackgroundView = v;
-        }
-        cell.opaque = YES;
-        
-        FXLabel *contactLabel = (FXLabel *) [cell viewWithTag:1];
-        contactLabel.textColor = [UIColor colorWithRed:11/255.0f green:67/255.0f blue:144/255.0f alpha:1];
-        
-        //contactLabel.textColor = [UIColor colorWithWhite:0.09f alpha:0.8f];
-        //contactLabel.highlightedTextColor =[UIColor blackColor];
-        //contactLabel.shadowColor = [UIColor colorWithWhite:0.9f alpha:0.8f];
-        //contactLabel.shadowOffset = CGSizeMake(0.8f, 0.80f);
-        //contactLabel.shadowBlur = 1.0f;
-        //contactLabel.innerShadowBlur = 3.0f;
-        //contactLabel.innerShadowColor = [UIColor colorWithRed:1/255.0f green:70/255.0f blue:148/255.0f alpha:1];
-        //
-        //contactLabel.innerShadowOffset = CGSizeMake(0.8f, 0.8f);
-        contactLabel.highlightedTextColor =[UIColor blackColor];
-        
-        UIImageView *img = (UIImageView *)[cell viewWithTag:2];
-        img.opaque = YES;
-        
-        if ([dataKey isEqualToString:@"pdf"]) {
-            [img setImage:[UIImage imageNamed:@"pdfImage"]];
-        }
-        else if ([dataKey isEqualToString:@"phone"]) {
-            [img setImage:[UIImage imageNamed:@"phone2"]];
-            contactLabel.text = @"Telefono";
-        }
-        else if ([dataKey isEqualToString:@"email"]) {
-            [img setImage:[UIImage imageNamed:@"mail2"]];
-            contactLabel.text = @"E-mail";
-        }
-        else{
-            [img setImage:nil];
-        }
-        
-        for(UIView *subview in [cell.contentView subviews]) {
-            if(subview.tag== 999) {
-                [subview removeFromSuperview];
-            }
-        }
-        //inserisco linea separatrice in top della prima cella
-        if(indexPath.row == 1){
-            CALayer *bottomBorder = [CALayer layer];
-            bottomBorder.frame = CGRectMake(0.0f, 0.0f,1024, 1.5f);
-            bottomBorder.backgroundColor = [UIColor whiteColor].CGColor;
-            
-            //linea separatrice alta 1px, posizionata alla base inferiore della cella
-            UIView *line = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1024, 1)];
-            line.opaque = YES;
-            line.tag = 999;
-            line.layer.borderColor = [UIColor colorWithRed:214/255.0f green:226/255.0f blue:241/255.0f alpha:1].CGColor;
-            line.layer.borderWidth = 1.0;
-            //applico bordo inferiore
-            [line.layer addSublayer:bottomBorder];
-            //applico linea alla cella
-            [cell.contentView addSubview:line];
-        }
-    }
-    else {
-        cell = [tableView dequeueReusableCellWithIdentifier:@"ActionCell"];
-        if (!cell) {
-            cell = [[[NSBundle mainBundle] loadNibNamed:@"ActionCell" owner:self options:NULL] objectAtIndex:0];
-            UIView *v = [[UIView alloc] init];
-            v.opaque = YES;
-            v.backgroundColor = [UIColor colorWithRed:194/255.0f green:203/255.0f blue:219/255.0f alpha:1];
-            cell.selectedBackgroundView = v;
-        }
-    }*/
-    
-    /*Testo della cella*//*
-    FXLabel *label = (FXLabel *) [cell viewWithTag:3];
-    label.textColor = [UIColor colorWithWhite:0.5f alpha:1];
-    label.shadowColor = [UIColor colorWithWhite:1.0f alpha:0.8f];
-    label.shadowOffset = CGSizeMake(0.8f, 0.80f);
-    label.shadowBlur = 1.0f;
-    label.innerShadowBlur = 3.0f;
-    label.innerShadowColor = [UIColor colorWithWhite:0.0f alpha:0.9f];
-    label.innerShadowOffset = CGSizeMake(0.8f, 0.8f);
-    label.highlightedTextColor =[UIColor blackColor];
-    label.text = [_dataModel valueForKey:@"LABEL" atIndexPath:indexPath];
-    
-    /* Linea separatrice tra le celle*//*
-    tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    //bordo inferiore da applicare alla linea
-    CALayer *bottomBorder = [CALayer layer];
-    bottomBorder.frame = CGRectMake(0.0f, 0.0f,1024, 1.5f);
-    bottomBorder.backgroundColor = [UIColor whiteColor].CGColor;
-    
-    //linea separatrice alta 1px, posizionata alla base inferiore della cella
-    UIView *separatorView = [[UIView alloc] initWithFrame:CGRectMake(0, 43, 1024, 1)];
-    separatorView.opaque = YES;
-    separatorView.layer.borderColor = [UIColor colorWithRed:214/255.0f green:226/255.0f blue:241/255.0f alpha:1].CGColor;
-    separatorView.layer.borderWidth = 1.0;
-    //applico bordo inferiore
-    [separatorView.layer addSublayer:bottomBorder];
-    //applico linea alla cella
-    [cell.contentView addSubview:separatorView];
-    /*Fine linea separatrice*/
-    
-    /*return cell;*/
-    return nil;
 }
 
 
@@ -206,9 +94,9 @@
 
 
 
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+/*- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
     [cell setBackgroundColor:[UIColor colorWithRed:246/255.0f green:250/255.0f blue:255/255.0f alpha:1]];
-}
+}*/
 
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -310,15 +198,24 @@
 }
 
 
+
 #pragma mark - ErrorView methods
 
--(void)stopSpinner{
+
+
+- (void)stopSpinner{
     [_spinner stopAnimating];
     [_spinner removeFromSuperview];
 }
 
 
 - (void)showErrorView:(NSString*)message {
+    if(_errorView && _errorView.showed){
+        return;
+    }
+
+    _errorView = [[ErrorView alloc] init];
+
     _errorView.label.text = message;
     [_errorView.tapRecognizer addTarget:self action:@selector(hideErrorView:)];
     
