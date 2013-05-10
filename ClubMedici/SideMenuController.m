0c228556508a794ec8947fc43e7071f6c4704b96
mario <mario@marios-Mac.local>
2013-05-10 14:51:32 +0200

    [SideMenuController]: prove grafiche

--- ClubMedici/SideMenuController.m
+++ ClubMedici/SideMenuController.m
@@ -59,11 +59,35 @@
     }
         
     cell.textLabel.text = [_dataModel valueForKey:@"LABEL" atIndexPath:indexPath];
+    NSLog(@"cell height = %f",cell.frame.size.height);
+    UIView* bgview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1, 1)];
+    bgview.opaque = YES;
+    bgview.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"side_menu_background"]];
+    [cell setBackgroundView:bgview];
     
+    //cell.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"side_menu_background"]];
     return cell;
 }
 
 
 #pragma mark - Table view delegate
+- (NSString*) tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return [_dataModel tableView:tableView titleForHeaderInSection:section];
+}
 
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    UIImageView *sectionView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"side_menu_sec_background"]];
+    sectionView.alpha = 0.95;
+    
+    //Add label to view
+    UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(13, -9, 320, 40)];
+    titleLabel.backgroundColor =[UIColor clearColor];
+    [titleLabel setFont: [UIFont fontWithName:@"Helvetica-Bold" size:16.5f ]];
+    titleLabel.textColor = [UIColor whiteColor];
+    titleLabel.text = [self tableView:tableView titleForHeaderInSection:section];
+    [sectionView addSubview:titleLabel];
+    
+    return sectionView;
+}
 @end
