4c735b6fe09f371b98aed1bc44e85a56003a40f2
mario <mario@marios-Mac.local>
2013-05-10 13:12:48 +0200

    [WebViewCell]: mod grafiche

--- ClubMedici/WebViewCell.m
+++ ClubMedici/WebViewCell.m
@@ -7,6 +7,7 @@
 //
 
 #import "WebViewCell.h"
+#import <QuartzCore/QuartzCore.h>
 
 @implementation WebViewCell
 
@@ -15,7 +16,7 @@
     if (self) {
         // Initialization code.
         self.frame = CGRectMake(0, 0, 320, 94);
-        CGRect webViewFrame = CGRectMake(0.0, 0.0, self.contentView.bounds.size.width, self.contentView.bounds.size.height);
+        CGRect webViewFrame = CGRectMake(0.0, 1.0, self.contentView.bounds.size.width, 92);
         webView = [[UIWebView alloc] initWithFrame:webViewFrame];
         webView.tag = 3;
         webView.userInteractionEnabled = NO;
@@ -24,6 +25,21 @@
         [self.contentView addSubview:webView];
         
         self.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+            CALayer *bottomBorder = [CALayer layer];
+            bottomBorder.frame = CGRectMake(0.0f, 0.0f,1024, 1.5f);
+            bottomBorder.backgroundColor = [UIColor whiteColor].CGColor;
+            
+            //linea separatrice alta 1px, posizionata alla base inferiore della cella
+            UIView *line = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1024, 1)];
+            line.opaque = YES;
+            line.tag = 999;
+            line.layer.borderColor = [UIColor colorWithRed:214/255.0f green:226/255.0f blue:241/255.0f alpha:1].CGColor;
+            line.layer.borderWidth = 1.0;
+            //applico bordo inferiore
+            [line.layer addSublayer:bottomBorder];
+            //applico linea alla cella
+            [self.contentView addSubview:line];
     }
     return self;
 }
